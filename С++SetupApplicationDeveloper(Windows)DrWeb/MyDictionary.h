a9e78043147a59e8be19015ff5d53edad1969769
Stanislav <petrov_stas@outlook.com>
2019-03-12 08:23:20 +0300

    Композиция была заменена на открытое наследование

--- С++SetupApplicationDeveloper(Windows)DrWeb/MyDictionary.h
+++ С++SetupApplicationDeveloper(Windows)DrWeb/MyDictionary.h
@@ -2,18 +2,52 @@
 #define DEFINE_MY_DICTIONARY
 
 #include "Dictionary.h"
-#include "TernaryTree.h"
-
 #include "MyNotFoundException.h"
 
 template<class TKey, class TValue>
 class MyDictionary: public Dictionary<TKey, TValue>
 {
 private:
-    TernaryTree<TKey, TValue> ternaryTree;
+    MyDictionary<TKey, TValue>* left;
+    MyDictionary<TKey, TValue>* right;
+    MyDictionary<TKey, TValue>* middle;
+
+    typename TKey::value_type split;
+    TValue value;
+
+    const TValue& MyGet(const TKey& key,
+        typename TKey::size_type i = 0) const;
+
+    
+    void MySet(const TKey& key, const TValue& value,
+        typename TKey::size_type i = 0);
+
+    bool MyIsSet(const TKey& key,
+        typename TKey::size_type i = 0) const;
 
 public:
-    virtual ~MyDictionary() = default;
+    inline MyDictionary() noexcept;
+
+    // Initialization of empty subtree.
+    MyDictionary(TKey const&, TValue const&,
+              typename TKey::size_type i = 0);
+
+    ~MyDictionary();
+
+    /*
+        On development stage.
+
+        TernaryTree(TernaryTree&);
+        TernaryTree(const TernaryTree&&);
+
+        TernaryTree& operator=(TernaryTree const&);
+        TernaryTree& operator=(TernaryTree&&);
+
+        void Swap(TernaryTree&);
+
+        PartialMatchSearch ???
+        PeighbourSearch ???
+    */
 
     virtual const TValue& Get(const TKey& key) const override;
     virtual void Set(const TKey& key, const TValue& value) override;
@@ -21,25 +55,146 @@ public:
 };
 
 template<class TKey, class TValue>
+inline MyDictionary<TKey, TValue>::MyDictionary() noexcept:
+    left{nullptr}, right{nullptr}, middle{nullptr}
+{
+    // To initialize fields with zero.
+}
+
+template<class TKey, class TValue>
+MyDictionary<TKey, TValue>::MyDictionary(TKey const& k, TValue const& v,
+    typename TKey::size_type i)
+{
+    split = k[i++];
+
+    if (i < k.size() )
+        middle = new MyDictionary<TKey, TValue>{k, v, i};
+    else
+        value = v;
+}
+
+template<class TKey, class TValue>
+MyDictionary<TKey, TValue>::~MyDictionary()
+{
+    delete left;
+    delete right;
+    delete middle;
+}
+
+template<class TKey, class TValue>
 const TValue& MyDictionary<TKey, TValue>::Get(const TKey& key) const
 {
-    const TValue* ret = ternaryTree.Search(key);
-    if (!ret)
-        throw MyNotFoundException<TKey>{key};
-
-    return *ret;
+    return MyGet(key);
 }
 
 template<class TKey, class TValue>
 void MyDictionary<TKey, TValue>::Set(const TKey& key, const TValue& value)
 {
-    ternaryTree.Insert(key, value);
+    MySet(key, value);
 }
 
 template<class TKey, class TValue>
 bool MyDictionary<TKey, TValue>::IsSet(const TKey& key) const
 {
-    return ternaryTree.Search(key);
+    return MyIsSet(key);
+}
+
+template<class TKey, class TValue>
+void MyDictionary<TKey, TValue>::MySet(TKey const& k, TValue const& v,
+    typename TKey::size_type i)
+{
+    if (k[i] < split)
+    {
+        if (left)
+            left->MySet(k, v);
+        else
+            left = new MyDictionary<TKey, TValue>{k, v};
+    }
+    else if (k[i] > split)
+    {
+        if (right)
+            right->MySet(k, v);
+        else
+            right = new MyDictionary<TKey, TValue>{k, v};
+    }
+    else
+    {
+        // Word root checking.
+        if (++i == k.size() )
+        {
+            if (value != v)
+                value = v;
+        }
+        else
+        {
+            if (middle)
+                middle->MySet(k, v, i);
+            else
+                middle = new MyDictionary<TKey, TValue>{k, v, i};
+        }
+    }
+}
+
+template<class TKey,class TValue>
+const TValue& MyDictionary<TKey,TValue>::MyGet(TKey const& k,
+    typename TKey::size_type i) const
+{
+    if (k[i] < split)
+    {
+        if (left)
+            return left->MyGet(k, i);
+    }
+    else if (k[i] > split)
+    {
+        if (right)
+            return right->MyGet(k, i);
+    }
+    else
+    {
+        if (++i == k.size() )
+        {
+            if (!value.empty() )
+                return value;
+        }
+        else
+        {
+            if (middle)
+                return middle->MyGet(k, i);
+        }
+    }
+
+    throw MyNotFoundException<TKey>{k};
+}
+
+template<class TKey,class TValue>
+inline bool MyDictionary<TKey,TValue>::MyIsSet(TKey const& k,
+    typename TKey::size_type i) const
+{
+    if (k[i] < split)
+    {
+        if (left)
+            return left->MyIsSet(k, i);
+    }
+    else if (k[i] > split)
+    {
+        if (right)
+            return right->MyIsSet(k, i);
+    }
+    else
+    {
+        if (++i == k.size() )
+        {
+            if (!value.empty() )
+                return true;
+        }
+        else
+        {
+            if (middle)
+                return middle->MyIsSet(k, i);
+        }
+    }
+
+    return false;
 }
 
 #endif
